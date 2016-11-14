24fd8bbb9622b10d383f3553a7a9a3e9d39378cc
peeyush b <pbishnoi@hortonworks.com>
2016-11-14 11:11:21 +0530

    Job instances listing after rebase

--- main/src/main/java/com/hortonworks/beacon/api/AbstractResourceManager.java
+++ main/src/main/java/com/hortonworks/beacon/api/AbstractResourceManager.java
@@ -24,6 +24,7 @@ import com.hortonworks.beacon.entity.util.EntityHelper;
 import com.hortonworks.beacon.entity.util.ReplicationPolicyBuilder;
 import com.hortonworks.beacon.exceptions.BeaconException;
 import com.hortonworks.beacon.replication.ReplicationJobDetails;
+import com.hortonworks.beacon.replication.ReplicationType;
 import com.hortonworks.beacon.scheduler.BeaconQuartzScheduler;
 import com.hortonworks.beacon.scheduler.BeaconScheduler;
 import com.hortonworks.beacon.store.bean.JobInstanceBean;
@@ -413,7 +414,8 @@ public abstract class AbstractResourceManager {
         if (policy != null) {
             // TODO process status and other query parameters
             BeaconScheduler scheduler = BeaconQuartzScheduler.get();
-            List<JobInstanceBean> instances = scheduler.listJob(entityName, policy.getType());
+            String replType = ReplicationType.valueOf(policy.getType()).getName();
+            List<JobInstanceBean> instances = scheduler.listJob(entityName, replType);
             return new JobInstanceList(instances);
         } else {
             throw new NoSuchElementException(entityName + " policy not found.");

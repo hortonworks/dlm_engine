81298c4a415a4ce3acf1a20a0f11958a36961bbc
Harkrishn Patro <hpatro@hortonworks.com>
2017-09-05 17:55:03 +0530

    BUG-87362: Load distcp filter class dynamically.
    Change-Id: Ief2ee9ea23539efe009d8e75b08b948c53c43b4f

---
+++ distcp/src/main/java/org/apache/hadoop/tools/DefaultFilter.java
@@ -0,0 +1,49 @@
+/**
+ *   Copyright  (c) 2016-2017, Hortonworks Inc.  All rights reserved.
+ *
+ *   Except as expressly permitted in a written agreement between you or your
+ *   company and Hortonworks, Inc. or an authorized affiliate or partner
+ *   thereof, any use, reproduction, modification, redistribution, sharing,
+ *   lending or other exploitation of all or any part of the contents of this
+ *   software is strictly prohibited.
+ */
+
+package org.apache.hadoop.tools;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * DefaultFilter extends the CopyFilter class, to filter out
+ * temporary file paths.
+ */
+public class DefaultFilter extends CopyFilter {
+    private static final Log LOG = LogFactory.getLog(DefaultFilter.class);
+
+    protected DefaultFilter(Configuration conf) {
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    public boolean shouldCopy(Path path) {
+        String fileName = path.getName();
+        String fileParentPath = path.getParent().toString();
+        if(fileParentPath.matches(".*\\/_temporary")
+                || fileParentPath.matches(".*\\/_temporary\\/.*")
+                || fileParentPath.matches(".*\\/\\._WIP_.*")
+                || fileParentPath.matches(".*\\/\\._WIP_.*\\/.*")
+                || fileName.endsWith("_COPYING")
+                || fileName.equals("_temporary")) {
+            LOG.info("Ignoring temporary file from being copied : " + path.toString());
+            return false;
+        }
+        return true;
+    }
+
+}

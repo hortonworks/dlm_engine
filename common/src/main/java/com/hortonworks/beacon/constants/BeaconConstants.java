20a6ef8939312f3e12721d3dbdca9afaba90a0cc
Venkat Ranganathan <venkat@hortonworks.com>
2017-09-28 17:28:32 -0700

    BUG-89126:  Remover ranger principal from validity requirements
    Change-Id: I0fc7c569089e41dca4e606e591c64484d0896660

--- common/src/main/java/com/hortonworks/beacon/constants/BeaconConstants.java
+++ common/src/main/java/com/hortonworks/beacon/constants/BeaconConstants.java
@@ -49,5 +49,4 @@ public final class BeaconConstants {
     public static final String SET = "SET ";
     public static final String HIVE_EXEC_PARALLEL = "hive.exec.parallel";
     public static final String HIVE_PRINCIPAL = "hive.server2.authentication.kerberos.principal";
-    public static final String RANGER_PRINCIPAL = "rangerPrincipalName";
 }

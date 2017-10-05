1b9e9c4e4adcff632b50e3c063e648f03f6d8fc9
Harkrishn Patro <hpatro@hortonworks.com>
2017-10-05 12:45:41 +0530

    BUG-89485: Fix resource bundle missing exception.
    Change-Id: I43dfc72094cc4a047335f7713d508286ef329f2b

--- distcp/src/test/java/org/apache/hadoop/tools/TestDefaultCopyFilter.java
+++ distcp/src/test/java/org/apache/hadoop/tools/TestDefaultCopyFilter.java
@@ -1,3 +1,13 @@
+/**
+ *   Copyright  (c) 2016-2017, Hortonworks Inc.  All rights reserved.
+ *
+ *   Except as expressly permitted in a written agreement between you or your
+ *   company and Hortonworks, Inc. or an authorized affiliate or partner
+ *   thereof, any use, reproduction, modification, redistribution, sharing,
+ *   lending or other exploitation of all or any part of the contents of this
+ *   software is strictly prohibited.
+ */
+
 package org.apache.hadoop.tools;
 
 import org.apache.hadoop.conf.Configuration;

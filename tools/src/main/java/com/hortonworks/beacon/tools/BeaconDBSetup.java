6c13fe5b3696fc84508df0bef6b3847642adddcb
Abhishek Bafna <abafna@hortonworks.com>
2017-12-26 10:29:02 +0530

    BUG-93757:  Define cloud credential schema and DB upgrade mechanism.
    Change-Id: Id9a26253620f8edf5df6b9219ad5b34a6e2a99bc

--- tools/src/main/java/com/hortonworks/beacon/tools/BeaconDBSetup.java
+++ tools/src/main/java/com/hortonworks/beacon/tools/BeaconDBSetup.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -44,81 +45,137 @@ public final class BeaconDBSetup {
     private static final String SCHEMA_FILE_PREFIX = "tables_";
     private static final String BEACON_SYS_TABLE =
             "create table beacon_sys (name varchar(40), data varchar(250))";
+    private static final String REPLACE_VERSION = "%SCHEMA_VERSION%";
     private static final String INSERT_SCHEMA_VERSION =
-            "insert into beacon_sys (name, data) values ('schema_version', '0.1')";
+            "insert into beacon_sys (name, data) values ('schema_version', '" + REPLACE_VERSION + "')";
+    private static final String UPDATE_SCHEMA_VERSION =
+            "update beacon_sys set data = '%SCHEMA_VERSION%' where name = 'schema_version'";
 
-    private BeaconDBSetup() {}
+    private static final ArrayList<String> SCHEMA_VERSIONS = new ArrayList<String>() {
+        {
+            add("0.1");
+            add("0.2");
+        }
+    };
+    private static final String SCHEMA_VERSION = SCHEMA_VERSIONS.get(SCHEMA_VERSIONS.size()-1);
 
-    public static void main(String[] args) {
+    private DbStore dbStore = BeaconConfig.getInstance().getDbStore();
+
+    private BeaconDBSetup() {
+    }
+
+    public static void main(String[] args) throws BeaconException, SQLException {
         setupDB();
     }
 
-    public static void setupDB() {
+    public static void setupDB() throws BeaconException, SQLException {
         BeaconDBSetup dbSetup = new BeaconDBSetup();
-        try {
+        try(Connection connection = dbSetup.getConnection(dbSetup.dbStore)) {
             LOGGER.info("Database setup is starting...");
-            BeaconConfig beaconConfig = BeaconConfig.getInstance();
-            String sqlFile = dbSetup.getSchemaFile(beaconConfig.getDbStore());
-            LOGGER.info("Setting up database with schema file: {0}", sqlFile);
-            dbSetup.setupBeaconDB(sqlFile);
+            String version = dbSetup.getSchemaVersion(connection);
+            if (StringUtils.isBlank(version)) {
+                String sqlFile = dbSetup.getSchemaFile(null);
+                dbSetup.bootstrap(connection, sqlFile);
+            } else if (!version.equalsIgnoreCase(SCHEMA_VERSION)){
+                LOGGER.info("Upgrading database current version [{}] to [{}]", version, SCHEMA_VERSION);
+                int index = SCHEMA_VERSIONS.indexOf(version);
+                for (int i = index+1; i<SCHEMA_VERSIONS.size(); i++) {
+                    String schemaFile = dbSetup.getSchemaFile(SCHEMA_VERSIONS.get(i));
+                    dbSetup.upgrade(connection, schemaFile);
+                }
+                dbSetup.updateVersion(connection, SCHEMA_VERSION);
+            } else {
+                LOGGER.info("Database schema is already setup with schema version [{}]", version);
+            }
             LOGGER.info("Database setup is completed.");
         } catch (Throwable e) {
-            LOGGER.error("Database setup failed with error {0}", e);
+            LOGGER.error("Database setup failed with error {}", e.getMessage(), e);
             System.exit(1);
         }
     }
 
-    private String getSchemaFile(DbStore store) {
-        String schemaDir = store.getSchemaDirectory();
+    private String getSchemaVersion(Connection connection) throws SQLException {
+        boolean exists = checkDatabaseExists(connection);
+        if (exists) {
+            String versionQuery = "Select data from beacon_sys where name=?";
+            try(PreparedStatement statement = connection.prepareStatement(versionQuery)) {
+                statement.setString(1, "schema_version");
+                try(ResultSet resultSet = statement.executeQuery()) {
+                    String version = null;
+                    if (resultSet.next()) {
+                        version = resultSet.getString("data");
+                    }
+                    return version;
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getSchemaFile(String version) {
+        String schemaDir = dbStore.getSchemaDirectory();
         if (StringUtils.isBlank(schemaDir)) {
             throw new IllegalArgumentException(
                 StringFormat.format("Schema directory does not exist: {}", schemaDir));
         }
-        String dbType = getDatabaseType(store);
-        File sqlFile = new File(schemaDir, SCHEMA_FILE_PREFIX + dbType + ".sql");
+        String dbType = getDatabaseType(dbStore);
+        String schemaFile;
+        if (StringUtils.isBlank(version)) {
+            schemaFile = SCHEMA_FILE_PREFIX + dbType + ".sql";
+        } else {
+            schemaFile = SCHEMA_FILE_PREFIX + dbType + "_" + version + ".sql";
+        }
+        File sqlFile = new File(schemaDir, schemaFile);
         if (!sqlFile.exists()) {
             throw new IllegalArgumentException(
                 StringFormat.format("Schema file does not exist: {}", sqlFile.getAbsolutePath()));
         }
+        LOGGER.info("Database schema file: [{}]", sqlFile.getAbsolutePath());
         return sqlFile.getAbsolutePath();
     }
 
-    private void setupBeaconDB(String sqlFile) throws BeaconException {
-        BeaconConfig beaconConfig = BeaconConfig.getInstance();
-        DbStore store = beaconConfig.getDbStore();
-        Connection connection = null;
+    private void bootstrap(Connection connection, String sqlFile) throws BeaconException {
         try {
-            connection = getConnection(store);
+            LOGGER.info("Setting up database with schema file: {}", sqlFile);
             List<String> queries = new ArrayList<>(getQueries(sqlFile));
-            boolean exists = checkDatabaseExists(connection);
-            if (!exists) {
-                LOGGER.info("Creating tables for the database...");
-                connection.setAutoCommit(false);
-                createSchema(connection, store);
-                queries.add(BEACON_SYS_TABLE);
-                createTables(connection, queries);
-                //TODO Later setup should check the version and update.
-                insertBeaconVersion(connection);
-                connection.commit();
-            } else {
-                LOGGER.info("Database setup is already done. Returning...");
-            }
+            LOGGER.info("Creating schema for the database...");
+            connection.setAutoCommit(false);
+            createSchema(connection, dbStore);
+            queries.add(BEACON_SYS_TABLE);
+            executeDDLs(connection, queries);
+            insertSchemaVersion(connection, SCHEMA_VERSION);
+            connection.commit();
         } catch (Throwable e) {
             throw new BeaconException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    // Ignore.
-                }
-            }
         }
     }
 
-    private void insertBeaconVersion(Connection connection) throws SQLException {
+    private void upgrade(Connection connection, String schemaFile) throws BeaconException {
+        try {
+            List<String> queries = new ArrayList<>(getQueries(schemaFile));
+            LOGGER.info("Upgrading schema for the database...");
+            connection.setAutoCommit(false);
+            executeDDLs(connection, queries);
+            connection.commit();
+        } catch (Exception e) {
+            throw new BeaconException(e);
+        }
+    }
+
+    private void insertSchemaVersion(Connection connection, String schemaVersion) throws SQLException {
         try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(INSERT_SCHEMA_VERSION);
+            String insertSchemaVersion = INSERT_SCHEMA_VERSION.replace(REPLACE_VERSION, schemaVersion);
+            statement.executeUpdate(insertSchemaVersion);
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    private void updateVersion(Connection connection, String schemaVersion) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String updateVersion = UPDATE_SCHEMA_VERSION.replace(REPLACE_VERSION, schemaVersion);
+            statement.executeUpdate(updateVersion);
         } catch (SQLException e) {
             LOGGER.error(e.getMessage(), e);
             throw e;
@@ -126,15 +183,16 @@ public final class BeaconDBSetup {
     }
 
     private boolean checkDatabaseExists(Connection connection) throws SQLException {
-        LOGGER.info("Checking database is already setup...");
+        LOGGER.info("Checking database schema is already setup...");
         DatabaseMetaData metaData = connection.getMetaData();
-        ResultSet resultSet = metaData.getTables(null, null, "%", null);
-        boolean exists = false;
-        while (resultSet.next() && !exists) {
-            String tableName = resultSet.getString(3);
-            exists = tableName.equalsIgnoreCase("beacon_sys");
+        try (ResultSet resultSet = metaData.getTables(null, null, "%", null)) {
+            boolean exists = false;
+            while (resultSet.next() && !exists) {
+                String tableName = resultSet.getString(3);
+                exists = tableName.equalsIgnoreCase("beacon_sys");
+            }
+            return exists;
         }
-        return exists;
     }
 
     private void createSchema(Connection connection, DbStore store) throws SQLException {
@@ -159,12 +217,12 @@ public final class BeaconDBSetup {
         return dbType;
     }
 
-    private void createTables(Connection connection, List<String> queries) throws SQLException {
+    private void executeDDLs(Connection connection, List<String> queries) throws SQLException {
         for (String query : queries) {
             try(Statement statement = connection.createStatement()) {
                 statement.execute(query);
             } catch (SQLException e) {
-                LOGGER.info("Failed table creation query: " + query);
+                LOGGER.info("Failed DDL query: " + query);
                 LOGGER.error("Error message: " + e.getMessage());
                 throw e;
             }

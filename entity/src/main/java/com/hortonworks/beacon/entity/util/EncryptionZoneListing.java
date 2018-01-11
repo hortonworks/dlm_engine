85b6794ac317d6fe77a36742707f1c3c54c60d7a
Harkrishn Patro <hpatro@hortonworks.com>
2018-01-11 11:38:20 +0530

    BUG-94360: Enable HDFS TDE replication based on source path encryption.
    Change-Id: Ib1780e6c0958f916f18f6212add4847ee3b5d0d7

--- entity/src/main/java/com/hortonworks/beacon/entity/util/EncryptionZoneListing.java
+++ entity/src/main/java/com/hortonworks/beacon/entity/util/EncryptionZoneListing.java
@@ -7,14 +7,12 @@
  *   lending or other exploitation of all or any part of the contents of this
  *   software is strictly prohibited.
  */
-package com.hortonworks.beacon.api;
+package com.hortonworks.beacon.entity.util;
 
-import com.hortonworks.beacon.client.entity.Cluster;
 import com.hortonworks.beacon.config.BeaconConfig;
 import com.hortonworks.beacon.constants.BeaconConstants;
 import com.hortonworks.beacon.exceptions.BeaconException;
 import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.hdfs.client.HdfsAdmin;
@@ -38,7 +36,7 @@ public final class EncryptionZoneListing {
 
     private static Map<String, Map<String, String>> encryptionZones = new ConcurrentHashMap<>();
     private static Map<String, Long> lastUpdated = new ConcurrentHashMap<>();
-    private static final Logger LOG = LoggerFactory.getLogger(DatasetListing.class);
+    private static final Logger LOG = LoggerFactory.getLogger(EncryptionZoneListing.class);
 
     private EncryptionZoneListing() {
     }
@@ -47,8 +45,10 @@ public final class EncryptionZoneListing {
         return INSTANCE;
     }
 
-    private void setEncryptedZones(Cluster cluster) throws URISyntaxException, IOException {
-        HdfsAdmin hdfsAdmin = new HdfsAdmin(new URI(cluster.getFsEndpoint()), new Configuration());
+    private void setEncryptedZones(String clusterName, String fsEndPoint) throws URISyntaxException, IOException,
+            BeaconException {
+        HdfsAdmin hdfsAdmin = new HdfsAdmin(new URI(fsEndPoint),
+                ClusterHelper.getHAConfigurationOrDefault(clusterName));
         RemoteIterator<EncryptionZone> iterator = hdfsAdmin.listEncryptionZones();
         Map<String, String> encryptionZonesLocal = new HashMap<>();
         while (iterator.hasNext()) {
@@ -57,49 +57,51 @@ public final class EncryptionZoneListing {
                     ? encryptionZone.getPath() :encryptionZone.getPath() + BeaconConstants.FORWARD_SLASH;
             encryptionZonesLocal.put(encryptionZonePath, encryptionZone.getKeyName());
         }
-        encryptionZones.put(cluster.getName(), encryptionZonesLocal);
-        lastUpdated.put(cluster.getName(), System.currentTimeMillis());
+        encryptionZones.put(clusterName, encryptionZonesLocal);
+        lastUpdated.put(clusterName, System.currentTimeMillis());
     }
 
-    public Map<String, String> getEncryptionZones(Cluster cluster, Path path) throws URISyntaxException,
-            IOException {
-        if (!encryptionZones.containsKey(cluster.getName()) || path.toString().equals(BeaconConstants.FORWARD_SLASH)
-                || isEncryptionZoneListValid(cluster.getName())) {
-            setEncryptedZones(cluster);
+    public Map<String, String> getEncryptionZones(String clusterName, String fsEndPoint, Path path) throws
+            URISyntaxException, IOException, BeaconException {
+        if (!encryptionZones.containsKey(clusterName) || path.toString().equals(BeaconConstants.FORWARD_SLASH)
+                || !isEncryptionZoneListValid(clusterName)) {
+            setEncryptedZones(clusterName, fsEndPoint);
         }
-        return encryptionZones.get(cluster.getName());
+        return encryptionZones.get(clusterName);
     }
 
-    public String getBaseEncryptedPath(Cluster cluster, String path) throws IOException, URISyntaxException,
+    public String getBaseEncryptedPath(String clusterName, String fsEndPoint, String path) throws IOException,
+            URISyntaxException,
             BeaconException {
         String encryptionKey = null;
         if (StringUtils.isNotEmpty(path)) {
-            encryptionKey = getBaseEncryptedPath(cluster, new Path(path));
+            encryptionKey = getBaseEncryptedPath(clusterName, fsEndPoint, new Path(path));
         }
         return encryptionKey;
     }
 
-    public String getEncryptionKeyName(Cluster cluster, String path) {
+    public String getEncryptionKeyName(String clusterName, String path) {
         String encryptionKeyName = "";
-        if (encryptionZones.containsKey(cluster.getName())) {
-            encryptionKeyName = encryptionZones.get(cluster.getName()).get(path);
+        if (encryptionZones.containsKey(clusterName)) {
+            encryptionKeyName = encryptionZones.get(clusterName).get(path);
         }
         return encryptionKeyName;
     }
 
     /**
      * This function returns the base path of a path if it is encrypted or else returns null.
-     * @param cluster
+     * @param clusterName
+     * @param fsEndPoint
      * @param path
-     * @return
+     * @return the base encrypted path, if available.
      * @throws IOException
      * @throws URISyntaxException
      * @throws BeaconException
      */
-
-    public String getBaseEncryptedPath(Cluster cluster, Path path) throws IOException, URISyntaxException,
+    public String getBaseEncryptedPath(String clusterName, String fsEndPoint, Path path) throws IOException,
+            URISyntaxException,
             BeaconException {
-        Map<String, String> currentEncryptionZones = getEncryptionZones(cluster, path);
+        Map<String, String> currentEncryptionZones = getEncryptionZones(clusterName, fsEndPoint, path);
         String pathToCheck = path.toUri().getPath();
         pathToCheck = pathToCheck.endsWith(BeaconConstants.FORWARD_SLASH)
                 ? pathToCheck : pathToCheck + BeaconConstants.FORWARD_SLASH;
@@ -130,10 +132,12 @@ public final class EncryptionZoneListing {
 
     private boolean isEncryptionZoneListValid(String clusterName) {
         boolean isValid = false;
-        if (lastUpdated.containsKey(clusterName) && (System.currentTimeMillis() - lastUpdated.get(clusterName))/1000
+        long elapsedTime = (System.currentTimeMillis() - lastUpdated.get(clusterName))/1000;
+        if (lastUpdated.containsKey(clusterName) && elapsedTime
                 < BeaconConfig.getInstance().getEngine().getRefreshEncryptionZones()) {
             isValid = true;
         }
+        LOG.debug("Encryption zone list last refreshed {} seconds ago. Valid: {}", elapsedTime, isValid);
         return isValid;
     }
 }
---
+++ entity/src/test/java/com/hortonworks/beacon/entity/util/EncryptionZoneListingTest.java
@@ -0,0 +1,65 @@
+/**
+ *   Copyright  (c) 2016-2017, Hortonworks Inc.  All rights reserved.
+ *
+ *   Except as expressly permitted in a written agreement between you or your
+ *   company and Hortonworks, Inc. or an authorized affiliate or partner
+ *   thereof, any use, reproduction, modification, redistribution, sharing,
+ *   lending or other exploitation of all or any part of the contents of this
+ *   software is strictly prohibited.
+ */
+
+package com.hortonworks.beacon.entity.util;
+
+import org.apache.hadoop.fs.Path;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Matchers.notNull;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+
+/**
+ * Test class for EncryptionZoneListing.
+ */
+@PrepareForTest(EncryptionZoneListing.class)
+public class EncryptionZoneListingTest extends PowerMockTestCase {
+
+    private EncryptionZoneListing encryptionZoneListing;
+
+    @BeforeClass
+    public void setup() {
+        encryptionZoneListing = PowerMockito.mock(EncryptionZoneListing.class);
+    }
+
+    @Test
+    public void testIsPathEncrypted() throws Exception {
+        Map<String, String> encryptionZones = new HashMap<>();
+        encryptionZones.put("/data/encrypt/", "default");
+        String clusterName = "src";
+        String fsEndPoint = "hdfs://localhost:8020";
+        when(encryptionZoneListing.getEncryptionZones((String) notNull(), (String) notNull(), (Path) notNull()))
+                .thenReturn(encryptionZones);
+        when(encryptionZoneListing.getEncryptionKeyName(clusterName, "/data/encrypt/")).thenReturn(
+                "default");
+        when(encryptionZoneListing.getBaseEncryptedPath((String) notNull(), (String) notNull(), (Path) notNull()))
+                .thenCallRealMethod();
+        when(encryptionZoneListing.getBaseEncryptedPath((String) notNull(), (String) notNull(), (String) notNull()))
+                .thenCallRealMethod();
+        Assert.assertEquals(encryptionZoneListing.getBaseEncryptedPath(clusterName, fsEndPoint,
+                new Path("/data/encrypt")), "/data/encrypt/");
+        Assert.assertEquals(encryptionZoneListing.getBaseEncryptedPath(clusterName, fsEndPoint,
+                new Path("/data/encrypt/media")), "/data/encrypt/");
+        Assert.assertNull(encryptionZoneListing.getBaseEncryptedPath(clusterName, fsEndPoint,
+                new Path("/data/encryptZone/media")));
+        Assert.assertNull(encryptionZoneListing.getBaseEncryptedPath(clusterName, fsEndPoint, new Path("/")));
+        Assert.assertNull(encryptionZoneListing.getBaseEncryptedPath(clusterName, fsEndPoint, new Path("/data")));
+        Assert.assertNull(encryptionZoneListing.getBaseEncryptedPath(clusterName, fsEndPoint, ""));
+    }
+}

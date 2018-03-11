b4dd2f590acf1a8cc268bc90e1333739a07c121c
Harkrishn Patro <hpatro@hortonworks.com>
2018-03-11 13:57:49 +0530

    BUG-98289: Add missing EncryptionAlgorithmType enum.
    Change-Id: Ic3ae2e1e5c327c4fa99f13768f7dbc31576b2df8

---
+++ common/src/main/java/com/hortonworks/beacon/EncryptionAlgorithmType.java
@@ -0,0 +1,44 @@
+/**
+ *   Copyright  (c) 2016-2017, Hortonworks Inc.  All rights reserved.
+ *
+ *   Except as expressly permitted in a written agreement between you or your
+ *   company and Hortonworks, Inc. or an authorized affiliate or partner
+ *   thereof, any use, reproduction, modification, redistribution, sharing,
+ *   lending or other exploitation of all or any part of the contents of this
+ *   software is strictly prohibited.
+ */
+
+package com.hortonworks.beacon;
+
+/**
+ * Cloud Object store encryption algorithm types.
+ */
+public enum EncryptionAlgorithmType {
+    AWS_AES256("AES256", "fs.s3a.server-side-encryption-algorithm"),
+    AWS_SSEKMS("SSE-KMS", "fs.s3a.server-side-encryption-algorithm");
+
+    private final String name;
+    private final String confName;
+
+    EncryptionAlgorithmType(String name, String confName) {
+        this.name = name;
+        this.confName = confName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getConfName() {
+        return confName;
+    }
+
+    public static EncryptionAlgorithmType fromName(String name) {
+        for (EncryptionAlgorithmType encryptionAlgorithmType : values()) {
+            if (name.equals(encryptionAlgorithmType.getName())) {
+                return encryptionAlgorithmType;
+            }
+        }
+        throw new IllegalArgumentException("Encryption algorithm type " + name + " not supported.");
+    }
+}
